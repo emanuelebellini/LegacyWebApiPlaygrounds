df7e48d27f0daaa5bd715b47c422993567978ebe
Emanuele Bellini <emanuelebellini@icloud.com>
2025-10-02 10:17:49 +0200

    Cleanup

--- DIPlayground/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DIPlayground.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DIPlayground/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DIPlayground.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
